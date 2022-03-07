05f2857d844d489f9d2ae281f1a06a5ee443bb05
Schwiegerknecht <74375857+Schwiegerknecht@users.noreply.github.com>
2022-03-08 00:01:13 +0100

    Oculus Sacrifice Fix
    Sacrifice now works as intended for allies.

---
+++ Hook/units/heroes/HOculus/HOculus_Abilities.lua
@@ -0,0 +1,170 @@
+# Fix Sacrifice so it actually works. Requires removing 'and not unit == v'.
+# Also uses the local allies variable from DivineJusticeBuffFunction because
+# the original would also heal towers which does not seem to be the intention.
+
+Ability.HOculusSacrifice01.AreaHeal = function(self, unit, data)
+    if data.Amount > self.MinHit then
+        Buff.ApplyBuff(unit, 'HOculusSacrificeDebuff01', unit)
+        
+        local HealAmt = data.Amount * self.PercentHeal
+        local pos = unit:GetPosition()
+        local radius = self.HealRadius
+
+                   
+        local data = {
+            Instigator = unit,
+            InstigatorBp = unit:GetBlueprint(),
+            InstigatorArmy = unit:GetArmy(),
+            Amount = HealAmt * -1,
+            IgnoreDamageRangePercent = true,
+            Type = 'Spell',
+            DamageAction = self.Name,
+            DamageSelf = false,
+            DamageFriendly = true,
+            Origin = pos,
+            CanDamageReturn = false,
+            CanCrit = false,
+            CanBackfire = false,
+            CanBeEvaded = false,
+            CanMagicResist = true,
+            ArmorImmune = true,
+            Group = "UNITS",
+        }
+        
+        # spell at center of Oculus
+        AttachEffectsAtBone( unit, EffectTemplates.Oculus.Sacrifice01, -2 )
+        
+        -- local entities = GetEntitiesInSphere(data.Group, pos, self.HealRadius )
+        -- entities = EntityCategoryFilterDown( categories.ALLUNITS - categories.UNTARGETABLE, entities )
+        -- Alternative from DivineJusticeBuffFunction so as not to heal towers
+        local aiBrain = unit:GetAIBrain()
+        local allies = aiBrain:GetUnitsAroundPoint( categories.MOBILE, unit.Position, self.HealRadius, 'Ally' )
+
+
+        for k,v in allies do
+            if IsAlly(unit:GetArmy(), v:GetArmy()) then -- 'and not unit == v' was the problem for some reason
+            -- Alternative from DivineJusticeBuffFunction:
+            -- if (v and not v:IsDead() and not EntityCategoryContains(categories.NOBUFFS, v) and not EntityCategoryContains(categories.UNTARGETABLE, v)) then
+                DealDamage(data, v)
+                
+                ## check to not play effects on oculus
+                if v != unit then
+                    # Heal effects at targeted unit
+                    AttachBuffEffectAtBone( v, 'Heal05', -2 );
+                end
+            end
+        end             
+    end
+end
+
+Ability.HOculusSacrifice02.AreaHeal = function(self, unit, data)
+    if data.Amount > self.MinHit then
+        Buff.ApplyBuff(unit, 'HOculusSacrificeDebuff01', unit)
+        
+        local HealAmt = data.Amount * self.PercentHeal
+        local pos = unit:GetPosition()
+        local radius = self.HealRadius
+
+                   
+        local data = {
+            Instigator = unit,
+            InstigatorBp = unit:GetBlueprint(),
+            InstigatorArmy = unit:GetArmy(),
+            Amount = HealAmt * -1,
+            IgnoreDamageRangePercent = true,
+            Type = 'Spell',
+            DamageAction = self.Name,
+            DamageSelf = false,
+            DamageFriendly = true,
+            Origin = pos,
+            CanDamageReturn = false,
+            CanCrit = false,
+            CanBackfire = false,
+            CanBeEvaded = false,
+            CanMagicResist = true,
+            ArmorImmune = true,
+            Group = "UNITS",
+        }
+        
+        # spell at center of Oculus
+        AttachEffectsAtBone( unit, EffectTemplates.Oculus.Sacrifice01, -2 )
+        
+        -- local entities = GetEntitiesInSphere(data.Group, pos, self.HealRadius )
+        -- entities = EntityCategoryFilterDown( categories.ALLUNITS - categories.UNTARGETABLE, entities )
+        -- Alternative from DivineJusticeBuffFunction so as not to heal towers
+        local aiBrain = unit:GetAIBrain()
+        local allies = aiBrain:GetUnitsAroundPoint( categories.MOBILE, unit.Position, self.HealRadius, 'Ally' )
+
+
+        for k,v in allies do
+            if IsAlly(unit:GetArmy(), v:GetArmy()) then -- 'and not unit == v' was the problem for some reason
+            -- Alternative from DivineJusticeBuffFunction:
+            -- if (v and not v:IsDead() and not EntityCategoryContains(categories.NOBUFFS, v) and not EntityCategoryContains(categories.UNTARGETABLE, v)) then
+                DealDamage(data, v)
+                
+                ## check to not play effects on oculus
+                if v != unit then
+                    # Heal effects at targeted unit
+                    AttachBuffEffectAtBone( v, 'Heal05', -2 );
+                end
+            end
+        end             
+    end
+end
+
+Ability.HOculusSacrifice03.AreaHeal = function(self, unit, data)
+    if data.Amount > self.MinHit then
+        Buff.ApplyBuff(unit, 'HOculusSacrificeDebuff01', unit)
+        
+        local HealAmt = data.Amount * self.PercentHeal
+        local pos = unit:GetPosition()
+        local radius = self.HealRadius
+
+                   
+        local data = {
+            Instigator = unit,
+            InstigatorBp = unit:GetBlueprint(),
+            InstigatorArmy = unit:GetArmy(),
+            Amount = HealAmt * -1,
+            IgnoreDamageRangePercent = true,
+            Type = 'Spell',
+            DamageAction = self.Name,
+            DamageSelf = false,
+            DamageFriendly = true,
+            Origin = pos,
+            CanDamageReturn = false,
+            CanCrit = false,
+            CanBackfire = false,
+            CanBeEvaded = false,
+            CanMagicResist = true,
+            ArmorImmune = true,
+            Group = "UNITS",
+        }
+        
+        # spell at center of Oculus
+        AttachEffectsAtBone( unit, EffectTemplates.Oculus.Sacrifice01, -2 )
+        
+        -- local entities = GetEntitiesInSphere(data.Group, pos, self.HealRadius )
+        -- entities = EntityCategoryFilterDown( categories.ALLUNITS - categories.UNTARGETABLE, entities )
+        -- Alternative from DivineJusticeBuffFunction so as not to heal towers
+        local aiBrain = unit:GetAIBrain()
+        local allies = aiBrain:GetUnitsAroundPoint( categories.MOBILE, unit.Position, self.HealRadius, 'Ally' )
+
+
+        for k,v in allies do
+            if IsAlly(unit:GetArmy(), v:GetArmy()) then -- 'and not unit == v' was the problem for some reason
+            -- Alternative from DivineJusticeBuffFunction:
+            -- if (v and not v:IsDead() and not EntityCategoryContains(categories.NOBUFFS, v) and not EntityCategoryContains(categories.UNTARGETABLE, v)) then
+                DealDamage(data, v)
+                
+                ## check to not play effects on oculus
+                if v != unit then
+                    # Heal effects at targeted unit
+                    AttachBuffEffectAtBone( v, 'Heal05', -2 );
+                end
+            end
+        end             
+    end
+end
+
+__moduleinfo.auto_reload = true
