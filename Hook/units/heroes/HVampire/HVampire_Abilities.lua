f6963314adf287ed7dddd3cca85499d516c9b143
Schwiegerknecht <he@fwegmann.com>
2022-04-09 16:13:51 +0200

    Army of the Night buffs working. Adjusting Vampiric aura description not working

--- Hook/units/heroes/HVampire/HVampire_Abilities.lua
+++ Hook/units/heroes/HVampire/HVampire_Abilities.lua
@@ -1,27 +1,19 @@
 --Reduce Batswarm II to Range 25 to make range increases per level consistent at 20,25,30
 Ability.HVampireBatSwarm02.RangeMax = 25
 
--- Schwiegerknecht
+
+-- Schwiegerknecht start
+
+-- Make description for Vampiric Aura more accurate
+Ability.HVampireVampiricAura01.GetLifeSteal = function(self) return math.floor( Buffs['HVampireVampiricAura01'].Affects.LifeSteal.Add * 100) end
+Ability.HVampireVampiricAura01.Description = 'Lord Erebus gains a [GetLifeSteal]% Life Steal Aura.'
+
 
 --Increase Army of the Night buffs: 40% Attack Speed (from 5) and + 100% Lifesteal (from 10%)
 Buffs.HVampireArmyoftheNight.Affects.RateOfFire.Mult = 0.4
 Buffs.HVampireArmyoftheNight.Affects.LifeSteal.Add = 1.0
 
-
--- Use an extra buff in between, otherwise Army of the Night would need to be an aura like Lightning Blast which contains a similar buff.
-
---[[This only works once for existing Night Crawlers, not new ones after adding ability. Use Coven function instead. Or maybe the ArmyBonus right away?
-Ability.HVampireArmyoftheNight.OnAbilityAdded = function(self, unit)
-    unit:GetAIBrain():AddArmyBonus( 'HVampireArmyoftheNight', self )
-    local vampirelings = ArmyBrains[unit:GetArmy()]:GetListOfUnits(categories.hvampirevampire01, false)
-    for k,vampireling in vampirelings do
-        Buff.ApplyBuff(vampireling, 'HVampireArmyoftheNightPlaceholder', unit)
-    end
-end
-]]
-
 -- Give Nightcrawlers a WeaponProc to reduce armor
-
 -- Add ability via Buff in the ArmyBonusBlueprint
 Buffs.HVampireArmyoftheNight.OnBuffAffect = function(self, unit, instigator)
     Abil.AddAbility(unit, 'HVampireArmyoftheNightMinionAbil', true)
@@ -32,9 +24,8 @@ AbilityBlueprint {
     DisplayName = 'Army of the Night Armor Shred',
     Description = 'Erebus\' Night Walkers have a [GetProcChance]% chance on each auto attack to reduce their targets armor.',
     GetProcChance = function(self) return math.floor( self.WeaponProcChance ) end,
-    --GetDuration = function(self) return string.format("%.1f", Buffs['HGSA01DeadeyeStun01'].Duration) end,
     AbilityType = 'WeaponProc',
-    WeaponProcChance = 100,
+    WeaponProcChance = 50,
     OnWeaponProc = function(self, unit, target, damageData)
         if EntityCategoryContains(categories.ALLUNITS, target) and not EntityCategoryContains(categories.UNTARGETABLE, target) then
             Buff.ApplyBuff(target, 'HVampireArmyoftheNightProcDebuff', unit)
@@ -49,21 +40,37 @@ AbilityBlueprint {
 BuffBlueprint {
     Name = 'HVampireArmyoftheNightProcDebuff',
     DisplayName = 'Army of the Night',
-    Description = 'WRECKED!',
+    Description = 'Armor reduced.',
     BuffType = 'HVAMPIREVAMPIREPROC',
     EntityCategory = 'ALLUNITS - UNTARGETABLE',
     Debuff = true,
     CanBeDispelled = true,
-    Stacks = 'REPLACE',
-    Duration = 5
+    Stacks = 'Always',
+    Duration = 5,
     Affects = {
-        Armor = {Add = -50},
+        Armor = {Add = -75},
     },
     Icon = '/DGVampLord/NewVamplordArmyoftheNight01',
 }
+-- Adjust description
+Ability.HVampireArmyoftheNight.GetLifeStealBonus = function(self) return math.floor( Buffs['HVampireArmyoftheNight'].Affects.LifeSteal.Add * 100 ) end
+Ability.HVampireArmyoftheNight.GetProcChance = function(self) return math.floor( Ability['HVampireArmyoftheNightMinionAbil'].WeaponProcChance ) end
+Ability.HVampireArmyoftheNight.GetArmorReduction = function(self) return math.floor( Buffs['HVampireArmyoftheNightProcDebuff'].Affects.Armor.Add * -1 ) end
 
-#TODO: Description
+Ability.HVampireArmyoftheNight.Description = 'Lord Erebus leads his Night Walkers as a bloodthirsty pack. Their Attack Speed is increased by [GetAttackBonus]% and they gain [GetLifeStealBonus]% lifesteal. They also have a [GetProcChance]% chance on auto attack to reduce the target\'s armor by [GetArmorReduction].'
 
+__moduleinfo.auto_reload = true
+
+
+--[[The below works once for existing Night Crawlers, not new ones after adding ability. Use Coven function instead. Or maybe the ArmyBonus right away?
+Ability.HVampireArmyoftheNight.OnAbilityAdded = function(self, unit)
+    unit:GetAIBrain():AddArmyBonus( 'HVampireArmyoftheNight', self )
+    local vampirelings = ArmyBrains[unit:GetArmy()]:GetListOfUnits(categories.hvampirevampire01, false)
+    for k,vampireling in vampirelings do
+        Buff.ApplyBuff(vampireling, 'HVampireArmyoftheNightPlaceholder', unit)
+    end
+end
+]]
 
 -- Testing if you can call the new class EliteCrawler. But you can't that easily.
 --[[
