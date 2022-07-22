d212b1ed05b21b3dc57a158bdae1946d5a4c90af
Schwiegerknecht <he@fwegmann.com>
2022-07-23 00:55:17 +0200

    Uproot; mod_info update; failed Entourage attempt

--- Hook/units/heroes/HQueen/HQueen_Abilities.lua
+++ Hook/units/heroes/HQueen/HQueen_Abilities.lua
@@ -135,11 +135,14 @@ end
 Ability.HQueenUproot04.GetStunDuration = function(self) return math.floor( Buffs['HQueenUprootStun04'].Duration) end
 Ability.HQueenUproot04.Description = 'Queen of Thorns sends her vines deep beneath the earth to destroy target structure\'s foundation, dealing [GetDamage] damage over [GetDuration] seconds. The structure is disabled for [GetStunDuration] seconds.'
 
---[[Incomplete idea:
---Add gold income buff to Entourage, ramping up to Tribute
+
+--[[
+
+-- Add gold income buff to Entourage (1/2/3 gold/sec), ramping up to Tribute.
+-- This Entourage buff requires the corresponding Summon Shambler level.
 BuffBlueprint {
     Name = 'HQueenEntourageTribute01',
-    BuffType = 'HQUEENENTOURAGEBUFF',
+    BuffType = 'HQUEENTRIBUTE',
     DisplayName = '<LOC ABILITY_Queen_0110>Entourage I Tribute',
     Description = '<LOC ABILITY_Queen_0111>Gold production increased.',
     Debuff = false,
@@ -151,7 +154,7 @@ BuffBlueprint {
 }
 BuffBlueprint {
     Name = 'HQueenEntourageTribute02',
-    BuffType = 'HQUEENENTOURAGEBUFF',
+    BuffType = 'HQUEENTRIBUTE',
     DisplayName = '<LOC ABILITY_Queen_0112>Entourage II Tribute',
     Description = '<LOC ABILITY_Queen_0113>Gold production increased.',
     Debuff = false,
@@ -163,7 +166,7 @@ BuffBlueprint {
 }
 BuffBlueprint {
     Name = 'HQueenEntourageTribute03',
-    BuffType = 'HQUEENENTOURAGEBUFF',
+    BuffType = 'HQUEENTRIBUTE',
     DisplayName = '<LOC ABILITY_Queen_0114>Entourage III Tribute',
     Description = '<LOC ABILITY_Queen_0115>Gold production increased.',
     Debuff = false,
@@ -173,11 +176,147 @@ BuffBlueprint {
         GoldProduction = {Add = 3},
     },
 }
+BuffBlueprint {
+    Name = 'HQueenEntourageTributeTest',
+    BuffType = 'HQUEENTRIBUTE',
+    DisplayName = '<LOC ABILITY_Queen_0114>Entourage III Tribute',
+    Description = '<LOC ABILITY_Queen_0115>Gold production increased.',
+    Debuff = false,
+    Stacks = 'REPLACE',
+    Duration = -1,
+    Affects = {
+        GoldProduction = {Add = 20},
+        MaxHealth = {Add = 3000, AdjustHealth = true},
+        Regen = {Add = 500},
+        EnergyRegen = {Add = 500},
+    },
+}
+-- Make Entourage gold buff dependent on having Summon Shambler.
+-- The level of Summon Shambler needs to be at least as high as the Entourage level to grant its gold bonus.
+-- E.g. if you have Entourage II and Summon Shambler III or vice versa, you would only gain the Entourage II buff.
+--[[TASKS:
+    - Check whether to apply buff whenever Summon Shambler (SS) or Entourage (Ent) is leveled (via OnAbilityAdded).
+      Always apply the buff corresponding to the smaller of the levels.
+      One possibility below, although you can also do it the other way round.
+    - leveling SS:
+        + For each SS level: Check whether current SS lvl <= Ent lvl.
+            - If yes, we can apply buff according to SS lvl.
+            - If not (so if SS > Ent), we apply buff according to Ent lvl.
+    - leveling Ent:
+        + For each Ent level: Check whether current Ent lvl <= SS lvl.
+            - If yes, we can apply buff according to Ent lvl.
+            - If not (so if Ent > SS), we apply buff according to SS lvl.]]
+
+--[[
+# NOT WORKING! Having Summon Shambler III or IV and then skilling Entourage I or II gives Entourage 3 buff.
+# So currently dependant only on Summon Shambler level.
+for i=1,3 do
+    Ability['HQueenEntourage0'..i].OnAbilityAdded = function(self, unit)
+        Entourage(self, 'HQueenEntourageBuff0'..i, unit)
+        for x=1,4 do
+            if Validate.HasAbility(unit, 'HQueenShambler0'..x) and x <= i then -- Check if Shambler lvl <= current Entourage lvl
+                Buff.ApplyBuff(unit, 'HQueenEntourageTribute0'..x, unit) -- Add Entourage buff matching Shambler lvl
+            elseif Validate.HasAbility(unit, 'HQueenShambler0'..x) and x > i then -- Check if Shambler lvl > current Entourage lvl
+                Buff.ApplyBuff(unit, 'HQueenEntourageTribute0'..i, unit) -- Add Entourage buff matching Entourage lvl
+            end
+        end
+    end
+end
+
+# This is working properly somehow
+Ability['HQueenEntourage0'..1].OnAbilityAdded = function(self, unit)
+    Entourage(self, 'HQueenEntourageBuff0'..1, unit)
+    for x=1,1 do -- Check for all Shambler lvls which are <= current Entourage lvl
+        if Validate.HasAbility(unit, 'HQueenShambler0'..x) then
+            Buff.ApplyBuff(unit, 'HQueenEntourageTribute0'..x, unit) -- Add Entourage buff matching Shmabler lvl
+        end
+    end
+    for x=2,4 do -- Check for all Shambler lvls which are > current Entourage lvl
+        if Validate.HasAbility(unit, 'HQueenShambler0'..x) then
+            Buff.ApplyBuff(unit, 'HQueenEntourageTribute0'..1, unit) -- Add buff of current Entourage lvl
+        end
+    end
+end
+Ability['HQueenEntourage0'..2].OnAbilityAdded = function(self, unit)
+    Entourage(self, 'HQueenEntourageBuff0'..2, unit)
+    for x=1,2 do -- Check for all Shambler lvls which are <= current Entourage lvl
+        if Validate.HasAbility(unit, 'HQueenShambler0'..x) then
+            Buff.ApplyBuff(unit, 'HQueenEntourageTribute0'..x, unit) -- Add Entourage buff matching Shmabler lvl
+        end
+    end
+    for x=3,4 do -- Check for all Shambler lvls which are > current Entourage lvl
+        if Validate.HasAbility(unit, 'HQueenShambler0'..x) then
+            Buff.ApplyBuff(unit, 'HQueenEntourageTribute0'..2, unit) -- Add buff of current Entourage lvl
+        end
+    end
+end
+Ability['HQueenEntourage0'..3].OnAbilityAdded = function(self, unit)
+    Entourage(self, 'HQueenEntourageBuff0'..3, unit)
+    for x=1,3 do -- Check for all Shambler lvls which are <= current Entourage lvl
+        if Validate.HasAbility(unit, 'HQueenShambler0'..x) then
+            Buff.ApplyBuff(unit, 'HQueenEntourageTribute0'..x, unit) -- Add Entourage buff matching Shmabler lvl
+        end
+    end
+    for x=4,4 do -- Check for all Shambler lvls which are > current Entourage lvl
+        if Validate.HasAbility(unit, 'HQueenShambler0'..x) then
+            Buff.ApplyBuff(unit, 'HQueenEntourageTribute0'..3, unit) -- Add buff of current Entourage lvl
+        end
+    end
+end
+
+
+-- Copy stuff from Uberfix so we don't overwrite it.
+# ?! This actually gets overwritten by Uberfix 1.06, despite after option in mod_info.lua ?!
+-- Summon Shambler applies Shambler buffs as army bonuses, so existing Shamblers will have their buffs upgraded with the ability increase.
+for i=2, 4 do
+    local abilShambler = Ability['HQueenShambler0'..i]
+    local buffNameShambler = 'HQueenShamblerBuff0'..i
+    if abilShambler and abilShambler.ShamblerBuff and Buffs[buffNameShambler] then -- always true
+        abilShambler.ShamblerBuff = nil
+        local armyBonusShambler = 'HQueenShamblerBonus0'..i
+        ArmyBonusBlueprint {
+            Name = armyBonusShambler,
+            ApplyArmies = 'Single',
+            UnitCategory = 'ENT',
+            RemoveOnUnitDeath = true,
+            Buffs = { buffNameShambler },
+        }
+        abilShambler.OnAbilityAdded = function(self, unit)
+            unit:GetAIBrain():AddArmyBonus(armyBonusShambler, unit)
+            -- Schwiegerknecht
+            # NOT WORKING! Having Entourage III and lvling Summon Shambler II gives Entourage 3 buff
+            # Also this gets overwritten by Uberfix 1.06. WHY?!?
+            for x = 1,3 do
+                if Validate.HasAbility(unit, 'HQueenEntourage0'..x) and x >= i then -- Check if Entourage lvl >= current Shambler lvl
+                    Buff.ApplyBuff(unit, 'HQueenEntourageTribute0'..i, unit) -- If so, give buff according to Shambler lvl.
+                elseif Validate.HasAbility(unit, 'HQueenEntourage0'..x) and x < i then -- Check if Entourage lvl < current Shambler lvl
+                    Buff.ApplyBuff(unit, 'HQueenEntourageTribute0'..x, unit) -- If so, give buff according to Entourage lvl
+                end
+            end
+        end
+        abilShambler.OnRemoveAbility = function(self, unit)
+            unit:GetAIBrain():RemoveArmyBonus(armyBonusShambler, unit)
+        end
+    end
+end
+-- Add the check for HQueenShambler01
+Ability.HQueenShambler01.OnAbilityAdded = function(self, unit)
+    for x = 1,3 do
+        if Validate.HasAbility(unit, 'HQueenEntourage0'..x) and x >= 1 then -- Check if Entourage lvl >= current Shambler lvl
+            Buff.ApplyBuff(unit, 'HQueenEntourageTribute01', unit) -- If so, give buff according to Shambler lvl
+        end
+    end
+end
+
 --Change Entourage descriptions
 Ability.HQueenEntourage01.GetEntourageGoldBuff1 = function(self) return math.floor( Buffs['HQueenEntourageTribute01'].Affects.GoldProduction.Add ) end
 Ability.HQueenEntourage02.GetEntourageGoldBuff2 = function(self) return math.floor( Buffs['HQueenEntourageTribute02'].Affects.GoldProduction.Add ) end
 Ability.HQueenEntourage03.GetEntourageGoldBuff3 = function(self) return math.floor( Buffs['HQueenEntourageTribute03'].Affects.GoldProduction.Add ) end
-Ability.HQueenEntourage01.Description = 'Increases Shambler Health by [GetShamblerHealth] and damage by [GetShamblerDamage]. Queen of Thorns demands tribute from her subjects. Gold production increased by [GetEntourageGoldBuff1] while Shamblers are alive.'
-Ability.HQueenEntourage02.Description = 'Increases Shambler Health by [GetShamblerHealth] and damage by [GetShamblerDamage]. Queen of Thorns demands tribute from her subjects. Gold production increased by [GetEntourageGoldBuff2] while Shamblers are alive.'
-Ability.HQueenEntourage03.Description = 'Increases Shambler Health by [GetShamblerHealth] and damage by [GetShamblerDamage]. Queen of Thorns demands tribute from her subjects. Gold production increased by [GetEntourageGoldBuff3] while Shamblers are alive.'
+# TODO: Use Pattern Matching to grab ability name only
+Ability.HQueenEntourage01.GetSummonShambler01Name = function(self) return Ability['HQueenShambler01'].DisplayName end
+Ability.HQueenEntourage02.GetSummonShambler02Name = function(self) return Ability['HQueenShambler02'].DisplayName end
+Ability.HQueenEntourage03.GetSummonShambler03Name = function(self) return Ability['HQueenShambler03'].DisplayName end
+Ability.HQueenEntourage01.Description = 'Increases Shambler Health by [GetShamblerHealth] and damage by [GetShamblerDamage]. Queen of Thorns demands tribute from her subjects. Gold production increased by [GetEntourageGoldBuff1] if she has at least the ability [GetSummonShambler01Name].'
+Ability.HQueenEntourage02.Description = 'Increases Shambler Health by [GetShamblerHealth] and damage by [GetShamblerDamage]. Queen of Thorns demands tribute from her subjects. Gold production increased by [GetEntourageGoldBuff2] if she has at least the ability [GetSummonShambler02Name].'
+Ability.HQueenEntourage03.Description = 'Increases Shambler Health by [GetShamblerHealth] and damage by [GetShamblerDamage]. Queen of Thorns demands tribute from her subjects. Gold production increased by [GetEntourageGoldBuff3] if she has at least the ability [GetSummonShambler03Name].'
 ]]
