bcd748b58abf4f9c5860d051eb2caf8211feab99
Schwiegerknecht <he@fwegmann.com>
2022-08-25 02:25:27 +0200

    Sludge slinger antiheal; Wards targetable?

--- Hook/lua/common/Items/Consumable_Items.lua
+++ Hook/lua/common/Items/Consumable_Items.lua
@@ -1,3 +1,7 @@
+-- Needed to give enemy an aura (Sludge Slinger)
+local Abil = import('/lua/sim/ability.lua')
+
+
 -- Make warpstone an instant cast down from 0.5 second cast time
 Ability.Item_Consumable_070.CastingTime = 0
 -- Increase Warlord punisher range to 20 up from 15
@@ -13,13 +17,107 @@ Ability.Item_Consumable_130.ChainAffectRadius = 8
 -- Set Warpstone Cooldown to 30 (from 45)
 Ability.Item_Consumable_070.Cooldown = 30
 
--- Increase Sludge Slinger attack speed reduction to 40% (from 30)
+-- Increase Sludge Slinger Attack Speed reduction to 40% (from 30)
 Buffs.Item_Consumable_040.Affects.RateOfFire = {Mult = -0.4}
--- Add 40% HP regen reduction (from 0)
+-- Increase Sludge Slinger Cooldown to 45 (from 30)
+Ability.Item_Consumable_040.Cooldown = 45
+--[[ Add 40% HP regen reduction (from 0)
 Buffs.Item_Consumable_040.Affects.Regen = {Mult = -0.5}
 Items.Item_Consumable_040.GetRegen = function(self) return math.floor( Buffs['Item_Consumable_040'].Affects.Regen.Mult * -100 ) end
 Buffs.Item_Consumable_040.Description = 'Attack Speed and Health per second reduced.'
 Items.Item_Consumable_040.Description = 'Use: Decreases target\'s Attack Speed by [GetAttackSpeedReduction]% and Health per second by [GetRegen]% for [GetDuration] seconds.'
+]]
+
+-- FX for Counterheal aura
+function FxHealingWind01( unit, trash )
+    local fx = AttachCharacterEffectsAtBone( unit, 'leopard_rider', 'HealingWind01', -2, trash )
+    for k, vEffect in fx do
+        unit.TrashOnKilled:Add(vEffect)
+    end
+end
+function FxHealingWind02( unit, trash )
+    local fx = AttachCharacterEffectsAtBone( unit, 'leopard_rider', 'HealingWind02', -2, trash )
+    for k, vEffect in fx do
+        unit.TrashOnKilled:Add(vEffect)
+    end
+end
+-- Counterheal aura added to enemies
+AbilityBlueprint {
+    Name = 'Item_Consumable_040_Counterheal',
+    DisplayName = '<LOC ITEM_Consumable_0090>Counter Healing',
+    Description = '<LOC ITEM_Consumable_0091>You counter the healing of allied priests around you.',
+    AbilityType = 'Aura',
+    AffectRadius = 15,
+    AuraPulseTime = 1,
+    TargetAlliance = 'Ally',
+    TargetCategory = 'PRIEST',
+    Tooltip = {
+        TargetAlliance = 'Ally',
+    },
+    Buffs = {
+        BuffBlueprint {
+            Name = 'SludgeCounterHealing01',
+            Debuff = true,
+            CanBeDispelled = true,
+            DisplayName = '<LOC ITEM_Consumable_0092>Counter Healing',
+            Description = '<LOC ITEM_Consumable_0093>Unable to heal.',
+            BuffType = 'SLUDGECOUNTERHEALING',
+            Stacks = 'REPLACE',
+            Duration = 1,
+            Icon = 'NewIcons/Scroll/Scroll2',
+            DoNotPulseIcon = true,
+            Affects = {
+                AbilityEnable = {Bool = true,},
+            },
+            Effects = 'CounterHeal01',
+            EffectsBone = -2,
+        },
+    },
+    CreateAbilityAmbients = function( self, unit, trash )
+        FxHealingWind01( unit, trash )
+        FxHealingWind02( unit, trash )
+    end,
+}
+--[[
+Buffs.Item_Consumable_040.OnBuffAffect = function(self, unit, instigator)
+    Abil.AddAbility(unit, 'Item_Consumable_040_Counterheal', true)
+end
+Buffs.Item_Consumable_040.OnBuffRemove = function(self, unit)
+    if unit.Sync.Abilities['Item_Consumable_040_Counterheal'] and not unit.Sync.Abilities['Item_Consumable_040_Counterheal'].Removed then
+        Abil.RemoveAbility(unit, 'Item_Consumable_040_Counterheal', true)
+    end
+end
+]]
+-- Dummy Buff giving opponent the Counterheal aura
+table.insert( Ability.Item_Consumable_040.Buffs, 1, BuffBlueprint {
+    Name = 'Item_Consumable_040_Dummy',
+    DisplayName = '<LOC ITEM_Consumable_0094>Sludge Slinger',
+    Description = '<LOC ITEM_Consumable_0095>Priest healing blocked.',
+    BuffType = 'SLUDGEDUMMY',
+    Debuff = true,
+    CanBeDispelled = true,
+    EntityCategory = 'MOBILE',
+    Stacks = 'REPLACE',
+    Duration = 10,
+    Affects = {
+        Dummy = {Add = 1},
+    },
+    Effects = 'Slow01',
+    EffectsBone = -2,
+    Icon = 'NewIcons/Scroll/Scroll2',
+    OnBuffAffect = function(self, unit, instigator)
+        Abil.AddAbility(unit, 'Item_Consumable_040_Counterheal', true)
+    end,
+    OnBuffRemove = function(self, unit)
+        if unit.Sync.Abilities['Item_Consumable_040_Counterheal'] and not unit.Sync.Abilities['Item_Consumable_040_Counterheal'].Removed then
+            Abil.RemoveAbility(unit, 'Item_Consumable_040_Counterheal', true)
+        end
+    end,
+})
+-- Adjust description
+Items.Item_Consumable_040.GetAuraDuration = function(self) return Buffs['Item_Consumable_040_Dummy'].Duration end
+Items.Item_Consumable_040.Description = 'Use: Decreases target\'s Attack Speed by [GetAttackSpeedReduction]% for [GetDuration] seconds. Also gives the target an aura that prevents priest healing for [GetAuraDuration] seconds.'
+
 
 -- Have Sludge Slinger reset Priest heal Cooldown
 # ISSUE: Not a debuff = not cleansable = bad
@@ -37,9 +135,9 @@ Items.Item_Consumable_040.Description = 'Use: Decreases target\'s Attack Speed b
 ]]
 
 
+
 -- Decrease Capture Lock duration to 30 seconds (from 45)
 Buffs.Item_Consumable_030.Duration = 30
-
 Ability.Item_Consumable_030.OnStartAbility = function(self, unit, params)
 
     ForkThread(
